86ef8922375023aa7b3566b0b9d68280b7a18a40
Infernalloo <infernodev00@gmail.com>
2023-08-14 10:17:28 +0200

    deleted gnome folder, merged DEs into one config

--- configuration.nix
+++ configuration.nix
@@ -49,6 +49,10 @@
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
+  # Enable the GNOME Desktop Environment.
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
