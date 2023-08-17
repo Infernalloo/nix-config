2e6e5b16db3034c2d8e7249d8d0369fe38ac98e1
Inferno <13460503-Infernalloo@users.noreply.gitlab.com>
2023-08-17 10:37:02 +0000

    Updated spacing and syntax

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -11,68 +7,65 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiSupport = true;
+    boot.loader.systemd-boot.enable = false;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.grub.device = "nodev";
+    boot.loader.grub.efiSupport = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  networking.networkmanager.enable = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Enabl networking
+    networking.hostName = "nixos"; # Define your hostname.
+    networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "Europe/Rome";
+    time.timeZone = "Europe/Rome";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+    i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "it_IT.UTF-8";
-    LC_IDENTIFICATION = "it_IT.UTF-8";
-    LC_MEASUREMENT = "it_IT.UTF-8";
-    LC_MONETARY = "it_IT.UTF-8";
-    LC_NAME = "it_IT.UTF-8";
-    LC_NUMERIC = "it_IT.UTF-8";
-    LC_PAPER = "it_IT.UTF-8";
-    LC_TELEPHONE = "it_IT.UTF-8";
-    LC_TIME = "it_IT.UTF-8";
-  };
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "it_IT.UTF-8";
+      LC_IDENTIFICATION = "it_IT.UTF-8";
+      LC_MEASUREMENT = "it_IT.UTF-8";
+      LC_MONETARY = "it_IT.UTF-8";
+      LC_NAME = "it_IT.UTF-8";
+      LC_NUMERIC = "it_IT.UTF-8";
+      LC_PAPER = "it_IT.UTF-8";
+      LC_TELEPHONE = "it_IT.UTF-8";
+      LC_TIME = "it_IT.UTF-8";
+    };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.displayManager.defaultSession = "plasmawayland";
+    services.xserver.enable = true;
+    services.xserver.displayManager.defaultSession = "plasmawayland";
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+    services.xserver.displayManager.sddm.enable = true;
+    services.xserver.desktopManager.plasma5.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
+    # services.xserver.displayManager.gdm.enable = true;
+    # services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
+    services.xserver = {
+      layout = "us";
+      xkbVariant = "";
+    };
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+    services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    sound.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -80,84 +73,82 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+    # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.inferno = {
-    isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" ];
-  };
+    users.users.inferno = {
+      isNormalUser = true;
+      extraGroups = [ "networkmanager" "wheel" ];
+    };
 
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+    nixpkgs.config.allowUnfree = true;
 
   # Enable flatpak
-  # services.flatpak.enable = true;
+    # services.flatpak.enable = true;
 
   # Enable virtualbox
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.guest.enable = true;
-  virtualisation.virtualbox.guest.x11 = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-  users.extraGroups.vboxusers.members = [ "inferno" ];
+    virtualisation.virtualbox.host.enable = true;
+    virtualisation.virtualbox.guest.enable = true;
+    virtualisation.virtualbox.guest.x11 = true;
+    virtualisation.virtualbox.host.enableExtensionPack = true;
+    users.extraGroups.vboxusers.members = [ "inferno" ];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  util-linux
-  vim
-  neovim
-  fish
-  git
-  exa
-  starship
-  neofetch
-  bunnyfetch
-  nitch
-  pfetch
-  bottom
-  vscode
-  spotify
-  alacritty
-  vivaldi
-  vivaldi-ffmpeg-codecs
-  lightly-qt
-  gparted
-  bitwarden
-  ];
+  # Packages to install system wide
+    environment.systemPackages = with pkgs; [
+      util-linux
+      vim
+      neovim
+      fish
+      git
+      exa
+      starship
+      neofetch
+      bunnyfetch
+      nitch
+      pfetch
+      bottom
+      vscode
+      spotify
+      alacritty
+      vivaldi
+      vivaldi-ffmpeg-codecs
+      lightly-qt
+      gparted
+      bitwarden
+    ];
 
   # Exclude some Plasma pkgs
-  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
-    elisa
-    oxygen
-    khelpcenter
-    kwallet
-    kwalletmanager
-    plasma-browser-integration
-  ];
+    environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+        elisa
+        oxygen
+        khelpcenter
+        kwallet
+        kwalletmanager
+        plasma-browser-integration
+    ];
 
-  fonts.fonts = with pkgs; [
-    (nerdfonts.override { fonts = ["JetBrainsMono" ]; })
-  ];
+  # Set the font
+    fonts.fonts = with pkgs; [
+      (nerdfonts.override { fonts = ["JetBrainsMono" ]; })
+    ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+    # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -165,6 +156,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+    system.stateVersion = "23.05"; # Did you read the comment?
 
 }
